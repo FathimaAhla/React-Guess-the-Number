6cdceed17bc5b9af7daf8ab9cc88352a38196fb7
Ahla Fayis <139907384+FathimaAhla@users.noreply.github.com>
2023-12-31 20:24:36 +0530

    feat(app.js): create secret number in app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Result from './Result';
+
+const secreNum = Math.floor(Math.random() * 10) + 1;
 
 function App() {
+  // temporary memory
+  const [term, setTerm] = useState("");
+
+  const handleChange = (e) => {
+      setTerm(e.target.value)
+  }
+
   return (
     <div className="App">
       <div className="container">
         <div className="head">
-          <label htmlFor='term'>Guess the number between 1 to 10</label>
+          <label htmlFor='term'>Guess the number between 1 to 10 </label>
         </div>
-        <input id='term' type='text' name='term' />
+        <input id='term' type='text' name='term' onChange={handleChange}/>
+        <Result secreNum={secreNum} term={term}/>
       </div>
     </div>
+
   );
 }
 
